5aa9c7ff36e93dc370e1754833e0bd468c9c3a78
erlenthi <erlenthi@stud.ntnu.no>
2020-12-17 11:43:16 +0100

    added everything

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -6,8 +6,46 @@ class EditUser extends LitElement {
       user: { type: Object }
     };
   }
+  render() {
+    return html` 
+    ${sessionStorage.getItem("name") ? 
+        html`<p>${sessionStorage.getItem("name") }</p>` : 
+        html``} 
+   
+   <form> 
+    <input type="text" name="email" placeholder ="Email"> 
+    <input type="password" name="pwd" placeholder = "New password">  
+    <input type="password" name="oldpwd" placeholder = "Old password">  
+    <input type="text" name="firstName" placeholder = "First Name">  
+    <input type="text" name="lastName" placeholder = "Last Name">  
+    <input type="text" name="uid" placeholder = "User id">  
+    <input type="text" name="uname" placeholder = "Username" required> 
+    <button @click="${this.update}" type="button" value="update">Update</button> <br>  
+   </form>  
 
-  // din kode her
+
+    `;  
+    
+  } 
+  update(e) {
+    const data = new FormData(e.target.form);
+
+    //Got this method for sending data from an example given earlier in the course
+    data.forEach((value, key) => object[key] = value); 
+    const newData = JSON.stringify(object);
+    fetch(`api/updateUser.php`, { 
+        method: 'POST',
+        credentials: "include", 
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: newData
+    }).then(res => res.json()
+    ).then(data => {
+        if(data.status=="success") console.log("Update complete."); 
+        else console.log("Error: " + data.msg);
+    });
+}  
 
 }
 customElements.define('edit-user', EditUser);
